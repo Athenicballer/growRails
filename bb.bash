cd944255d82c18d6d90615ae2b3dce500b259d4b
Athenicballer <Athenanettech@gmail.com>
2025-10-09 11:40:37 +0100

    .

--- bb.bash
+++ bb.bash
@@ -4,5 +4,7 @@ mkdir build
 cd build
 
 # Use the toolchain file as before
-cmake -DCMAKE_TOOLCHAIN_FILE=/home/user/rpi-toolchain.cmake .. 
+# cmake -DCMAKE_TOOLCHAIN_FILE=/home/user/rpi-toolchain.cmake .. 
+cmake -DCMAKE_TOOLCHAIN_FILE=/home/user/source/rpi-toolchain.cmake .. 
+
 make
