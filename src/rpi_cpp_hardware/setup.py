6276c3705c24a392107bdf2dcb00cb90cb060e8c
Athenicballer <Athenanettech@gmail.com>
2025-10-15 22:14:02 +0100

    s

--- src/rpi_cpp_hardware/setup.py
+++ src/rpi_cpp_hardware/setup.py
@@ -7,19 +7,23 @@ package_name = 'rpi_cpp_hardware'
 setup(
     name=package_name,
     version='0.0.0',
-    # We leave packages=[] empty because this package installs C++ executables, not Python modules.
+    # We leave packages=[] empty because this package installs C++ executables
     packages=[],
-    
-    # --- CRITICAL SECTION: Installing the Launch File ---
     data_files=[
+        # Install resources (package.xml and resource index)
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-        # This line installs your launch file into the package's 'launch' directory
-        (os.path.join('share', package_name, 'launch'), glob(os.path.join('launch', 'rpi_system_launch.py'))),
-    ],
-    # ----------------------------------------------------
+        
+        # --- CRITICAL SECTION: Installing the Launch File ---
+        # Install the Python launch file directly into the share/package_name directory.
+        # This is the correct location for Python launch files to be found by `ros2 launch`.
+        (os.path.join('share', package_name), glob(os.path.join('launch', '*.launch.py'))),
+        
+        # This line was unnecessary and has been removed:
+        # (os.path.join('share', package_name, 'launch'), glob(os.path.join('launch', '*.launch.py'))),
 
+    ],
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='grower',
@@ -27,7 +31,6 @@ setup(
     description='ROS 2 hardware package for RPi robot control.',
     license='TODO: License declaration',
     tests_require=['pytest'],
-    # No console scripts needed here since the C++ executables are handled by CMake
     entry_points={
         'console_scripts': [],
     },
