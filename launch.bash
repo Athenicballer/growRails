fa8b404e624a64cb1563cb8d6a8758c4b41f973e
Athenicballer <Athenanettech@gmail.com>
2025-10-15 13:46:12 +0100

    s

--- launch.bash
+++ launch.bash
@@ -1,3 +1,13 @@
-colcon build --packages-select rpi_cpp_hardware
+#!/bin/bash
+
+# --- NEW LINE ADDED TO SOURCE ROS 2 ENVIRONMENT ---
+source /opt/ros/jazzy/setup.bash
+
+# Build the package if it hasn't been built yet
+colcon build --packages-select rpi_cpp_hardware || exit 1
+
+# Source the package (this is needed to find the local launch file)
 source install/setup.bash
-ros2 launch rpi_cpp_hardware rpi_system_launch.py
+
+# Launch the node
+ros2 launch rpi_cpp_hardware rpi_cpp_hardware_launch.py
